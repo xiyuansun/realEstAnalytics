9053e2f3b297aef61eb3293b48780a4b7de10a87
GaniAgadil <agadilov@iastate.edu>
2019-05-02 17:39:48 -0500

    update testing functions

--- tests/testthat/test-set_zillow_web_service_id.R
+++ tests/testthat/test-set_zillow_web_service_id.R
@@ -1,5 +1,13 @@
 
-context('set_zillow_web_service_id')
+context('zillow_web_service_id')
+
+
+test2 <- test_that(" provide the correct input", {
+  # expect an error due to incorrect input
+  expect_error(get_zillow_web_service_id(x= abc))
+  # expect an error due to incorrect input
+  expect_error(get_zillow_web_service_id(x=a1b2c3))
+})
 
 test_that(" provide the correct input", {
   # expect an error due to incorrect input
