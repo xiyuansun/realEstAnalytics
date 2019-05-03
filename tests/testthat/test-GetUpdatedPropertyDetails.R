106fd6e5894aeeada646af805d7108f7cce35e98
Xiyuan Sun <xiyuansu@iastate.edu>
2019-05-02 20:56:17 -0500

    deleted the test part for helper functions

--- tests/testthat/test-GetUpdatedPropertyDetails.R
+++ tests/testthat/test-GetUpdatedPropertyDetails.R
@@ -41,36 +41,4 @@ test_that(" provide the correct input", {
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key= zapi_key,raw=a1b2c3))
 })
 
-test_that(" provide the correct input", {
-  # expect an error due to incorrect input
-  expect_error(extract_editedfacts(abc))
-  # expect an error due to incorrect input
-  expect_error(extract_editedfacts(a1b2c3))
-})
-
-test_that(" provide the correct input", {
-  # expect an error due to incorrect input
-  expect_error(extract_pageview(abc))
-  # expect an error due to incorrect input
-  expect_error(extract_pageview(a1b2c3))
-})
-
-test_that(" provide the correct input", {
-  # expect an error due to incorrect input
-  expect_error(extract_address_e(c(abc, "zipcode", "city", "state", "lat","long")))
-  # expect an error due to incorrect input
-  expect_error(extract_address_e(c("address", abc, "city", "state", "lat","long")))
-  # expect an error due to incorrect input
-  expect_error(extract_address_e(c("address", "zipcode", abc, "state", "lat","long")))
-  # expect an error due to incorrect input
-  expect_error(extract_address_e(c("address", "zipcode", "city", abc, "lat","long")))
-  # expect an error due to incorrect input
-  expect_error(extract_address_e(c("address", "zipcode", "city", "state", abc,"long")))
-  # expect an error due to incorrect input
-  expect_error(extract_address_e(c("address", "zipcode", "city", "state", "lat",abc)))
-})
-#test_that(" output is a list", {
-  # expect list
-#  expect_is(GetUpdatedPropertyDetails(zpid = 48749425,api_key = 'X1-ZWz181enkd4cgb_82rpe'), "list")
-#})
 
