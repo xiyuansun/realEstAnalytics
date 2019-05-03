606b28ac5354ff6b0966e2817330746c29e7c402
Xiyuan Sun <xiyuansu@iastate.edu>
2019-05-02 23:12:26 -0500

    tried new test GetUpdatedPropertyDetails

--- tests/testthat/test-GetUpdatedPropertyDetails.R
+++ tests/testthat/test-GetUpdatedPropertyDetails.R
@@ -5,36 +5,30 @@ set_zillow_web_service_id('X1-ZWz181enkd4cgb_82rpe')
 zws_id = getOption('ZillowR-zws_id')
 zapi_key = getOption('ZillowR-zws_id')
 
-test_that(" provide the correct adress", {
+test_that(" provide the correct zillow property ID", {
   expect_error(GetUpdatedPropertyDetails(zpid = abc,api_key = zapi_key,raw=FALSE))
-  expect_error(GetUpdatedPropertyDetails(zpid = a1b2c3,api_key = zapi_key,raw=FALSE))
+  #identical(GetUpdatedPropertyDetails(zpid = '0000000',api_key = zapi_key,raw=FALSE),NA)
 })
 
-test_that(" provide the correct zillow property ID for which to search", {
-  expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key = abc,raw=FALSE))
-  expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key= a1b2c3,raw=FALSE))
+test_that(" provide the correct API key", {
+  #identical(GetUpdatedPropertyDetails(zpid = '93961896',api_key = 'abc',raw=FALSE),NA)
+  expect_error(GetUpdatedPropertyDetails(zpid = '93961896',api_key= a1b2c3,raw=FALSE))
+  identical(ncol(GetUpdatedPropertyDetails(zpid='93961896' ,api_key= zapi_key)),23)
 })
 
 
 test_that(" provide the correct adress", {
-  # expect an error due to incorrect address
   expect_error(GetUpdatedPropertyDetails(zpid = abc,api_key = zapi_key,raw=TRUE))
-  # expect an error due to incorrect adress
   expect_error(GetUpdatedPropertyDetails(zpid = a1b2c3,api_key = zapi_key,raw=TRUE))
 })
 
-test_that(" provide the correct ZIP code for which to search", {
-  # expect an error due to incorrect citystatezip
+test_that(" provide the correct API key", {
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key = abc,raw=TRUE))
-  # expect an error due to incorrect citystatezip
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key= a1b2c3,raw=TRUE))
 })
 
-test_that(" provide the correct input", {
-  # expect an error due to incorrect input
+test_that(" provide the correct raw status", {
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key = zapi_key,raw=abc))
-  # expect an error due to incorrect input
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key= zapi_key,raw=a1b2c3))
 })
-
 
