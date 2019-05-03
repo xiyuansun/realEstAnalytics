974081ed34a15542e7e09d24c96684d3f90367e8
Xiyuan Sun <xiyuansu@iastate.edu>
2019-05-02 21:59:27 -0500

    added another test for url_encode_request

--- tests/testthat/test-GetUpdatedPropertyDetails.R
+++ tests/testthat/test-GetUpdatedPropertyDetails.R
@@ -6,16 +6,12 @@ zws_id = getOption('ZillowR-zws_id')
 zapi_key = getOption('ZillowR-zws_id')
 
 test_that(" provide the correct adress", {
-  # expect an error due to incorrect address
   expect_error(GetUpdatedPropertyDetails(zpid = abc,api_key = zapi_key,raw=FALSE))
-  # expect an error due to incorrect adress
   expect_error(GetUpdatedPropertyDetails(zpid = a1b2c3,api_key = zapi_key,raw=FALSE))
 })
 
-test_that(" provide the correct ZIP code for which to search", {
-  # expect an error due to incorrect citystatezip
+test_that(" provide the correct zillow property ID for which to search", {
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key = abc,raw=FALSE))
-  # expect an error due to incorrect citystatezip
   expect_error(GetUpdatedPropertyDetails(zpid = '48749425',api_key= a1b2c3,raw=FALSE))
 })
 
--- tests/testthat/test-url_encode_request.R
+++ tests/testthat/test-url_encode_request.R
@@ -2,9 +2,19 @@
 context('url_encode_request')
 
 test_that(" provide the correct input", {
-  # expect an error due to incorrect input
   expect_error(url_encode_request(x= abc,...))
-  # expect an error due to incorrect input
   expect_error(url_encode_request(x=a1b2c3,...))
+
+})
+
+test_that("function creates proper URL request string", {
+  absent_arg <- NULL
+  character_arg <- 'Here I am!'
+  numeric_arg <- 100
+
+  expect_identical(
+    url_encode_request('http://example.com/API.htm', absent_arg = absent_arg, character_arg = character_arg, numeric_arg = numeric_arg),
+    'http://example.com/API.htm?character_arg=Here%20I%20am!&numeric_arg=100'
+  )
 })
 
